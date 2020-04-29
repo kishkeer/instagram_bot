8282d167aef16c9a9b56fa93f976aa284a23411c
Kishoore K <47095121+kishkeer@users.noreply.github.com>
2020-04-29 08:12:33 +0530

    Update insta_autoliker&follower.py

--- insta_autoliker&follower.py
+++ insta_autoliker&follower.py
@@ -15,7 +15,7 @@ CONFIGS = {
 total_likes = 0
 total_follows=0
 
-driver = webdriver.Chrome(r'C:\Users\HP\Desktop\chromedriver.exe')
+driver = webdriver.Chrome(r'C:\Users\HP\Desktop\chromedriver.exe')#enter the path of chromedriver
 driver.get("https://www.instagram.com/")
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((   By.NAME, 'username')))
 usernameInput = driver.find_element(By.NAME, 'username')
@@ -47,39 +47,39 @@ for current_hashtag in CONFIGS['HASHTAGS']:
 
     current_hashtag_likes = 0
     current_follows=0
-    while current_hashtag_likes < CONFIGS['TOTAL_LIKES_PER_HASHTAG']:
-     WebDriverWait(driver, 10).until(EC.presence_of_element_located((  By.TAG_NAME, "time" )))
-     time.sleep(1)
+   while current_hashtag_likes < CONFIGS['TOTAL_LIKES_PER_HASHTAG']:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "time")))
+        time.sleep(1)
+        likeButton = driver.find_element(By.XPATH,
+                                         "/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button")
+        likeButton.click()
+        time.sleep(3)
 
+        follower = driver.find_element(By.XPATH,
+                                       "/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[2]/button")
+        if follower.text == "Follow":
+           follower.click()
+           total_follows = total_follows + 1
+           current_follows = current_follows + 1
+        time.sleep(3)
 
-     likeButton = driver.find_element(  By.XPATH, "/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button"  )
-     likeButton.click()
-     time.sleep(2)
+        print("[#{}/{}/{}/no of follows:{}]".format(
+            current_hashtag,
+            current_hashtag_likes,
+            CONFIGS['TOTAL_LIKES_PER_HASHTAG'],
+            current_follows
+        ))
 
-     follower=driver.find_element(By.XPATH,"/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[2]/button").click()
-     time.sleep(2)
+        total_likes = total_likes + 1
+        current_hashtag_likes = current_hashtag_likes + 1
 
+        time.sleep(10)
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'coreSpriteRightPaginationArrow')))
 
-     print("[#{}/{}/{}/no of follows:{}]".format(
-                current_hashtag,
-                current_hashtag_likes,
-                CONFIGS['TOTAL_LIKES_PER_HASHTAG'],
-                current_follows
-            ))
+        nextButton = driver.find_element(By.CLASS_NAME, 'coreSpriteRightPaginationArrow')
 
-     total_likes = total_likes + 1
-     current_hashtag_likes = current_hashtag_likes + 1
-     total_follows=total_follows+1
-     current_follows=current_follows+1
-     time.sleep(10)
-
-
-
-     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'coreSpriteRightPaginationArrow' )))
-
-     nextButton = driver.find_element(By.CLASS_NAME, 'coreSpriteRightPaginationArrow')
-
-     nextButton.click()
+        nextButton.click()
 
 print("Exiting...")
 print("=====================")
